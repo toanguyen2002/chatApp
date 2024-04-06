f80a1a474ecdc5dba0b565019b58ff0ec8df7654
Quang Toan <toanguyen200220@gmail.com>
2024-04-06 17:38:47 +0700

    add chuc nang thu hoi + xoa

--- server/Router/mesRouter.js
+++ server/Router/mesRouter.js
@@ -6,12 +6,14 @@ const upload = multer({ storage })
 
 const router = express.Router()
 
-const { sendMessage, allMessages, sendMessImage } = require("../Controllers/MessageController")
+const { sendMessage, allMessages, sendMessImage, blacnkMess, deleteMess } = require("../Controllers/MessageController")
 const { protect } = require("../middleware/auth")
 
 
 router.route("/:chatId").get(protect, allMessages)
 router.route("/").post(protect, sendMessage)
+router.route("/blankMess").post(protect, blacnkMess)
+router.route("/deleteMess").post(protect, deleteMess)
 router.post("/messImage", upload.single("fileImage"), sendMessImage)
 
 
